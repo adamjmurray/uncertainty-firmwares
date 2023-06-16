3478806dd3af6c97476e1a2c8bc3e8b42ccd20d0
Adam Murray <adamjmurray@gmail.com>
2023-06-16 11:08:32 -0700

    stepper firmware is working

--- micropython/stepper.py
+++ micropython/stepper.py
@@ -1,106 +1,38 @@
-######################
-# lib.py
-
 import machine
-from machine import ADC, Pin, Timer
-from collections import deque
-from enum import Enum
-
-cv_in = ADC(Pin(26))
-
-outs = [Pin(number, Pin.OUT) for number in [27, 28, 29, 0, 3, 4, 2, 1]]
-
-num_outs = len(outs)
-
-
-# Read from the CV input.
-# Returns a number in the clamped range (-1, 1) over approx. -5V to +5V of input voltage.
-def read_cv():
-    # different modules may need different calibration here
-    normalized = (cv_in.read_u16() - 3470) / 65500
-    clamped = min(max(normalized, 0), 1)
-    return 2 * clamped - 1
-
-
-def write_out(index, value):
-    try:
-        outs[index].value(value)
-    except IndexError:
-        print("Invalid index %s" % index)
-
-
-######################
-# More lib
-
-class SimpleLPF:
-    def __init__(self, weight=0.5):
-        self.weight = weight
-        self.prev = 0
-
-    def filter(self, sample):
-        self.prev = self.weight * self.prev + (1 - self.weight) * sample
-        return self.prev
-
-
-class BipolarTrigger:
-    State = Enum('BipolarTriggerState', ['OFF', 'RISING', 'ON', 'FALLING'])
-
-    def __init__(self, rising_thresh=0.2, falling_thresh=0.1, rising_window=100, falling_window=100):
-        self.rising_thresh = rising_thresh
-        self.falling_thresh = falling_thresh
-        self.rising_window = rising_window
-        self.falling_window = falling_window
-        self.state = BipolarTrigger.State.OFF
-        self.countdown = 0
-        self.lpf = SimpleLPF(0.1)
-
-    def detect(self, sample):
-        filtered = self.lpf.filter(sample)
-        absval = abs(filtered)
-
-        if self.state == BipolarTrigger.State.OFF:
-            if absval > self.rising_thresh:
-                self.state = BipolarTrigger.State.RISING
-                self.countdown = self.rising_window
-
-        elif self.state == BipolarTrigger.State.RISING:
-            if absval > self.rising_thresh:
-                self.countdown -= 1
-                if self.countdown < 0:
-                    self.state = BipolarTrigger.State.ON
-                    # the only time this returns anything other than zero is the moment we transition to the ON state:
-                    return filtered
-
-            else:
-                self.state = BipolarTrigger.State.OFF
-
-        elif self.state == BipolarTrigger.State.ON:
-            if absval < self.falling_thresh:
-                self.state = BipolarTrigger.State.FALLING
-                self.countdown = self.falling_window
-
-        elif self.state == BipolarTrigger.State.FALLING:
-            if absval < self.falling_thresh:
-                self.countdown -= 1
-                if self.countdown < 0:
-                    self.state = BipolarTrigger.State.OFF
-            else:
-                self.state = BipolarTrigger.State.ON
-
-        # if we didn't transition to ON, we always return 0
-        return 0
-
-
-######################
-# Stepper firmeware
+from lib.io import read_cv, output, num_outs
+from lib.dsp import BipolarTrigger
 
 machine.freq(250000000)  # 2x default
-trigger = BipolarTrigger(rising_thresh=0.25)
+
+trigger = BipolarTrigger(rising_thresh=1.9)
 active_gate = 0
 
 while True:
-    offset = int(trigger.detect(read_cv())/0.25)
-    if offset > 0:
+    offset = 0
+    volts = 5 * read_cv()
+    trig = trigger.detect(volts)
+
+    # fine-tuning a bit because read_cv() is not perfectly normalized
+    # This will advance by 1 around 2V, by 2 at 3V, by 3 at 4V, and by 4 at 5V
+    if trig > 4.7:  # TODO: why is this so low, do we need to recalibrate?
+        offset = 4
+    elif trig > 3.9:
+        offset = 3
+    elif trig > 2.9:
+        offset = 2
+    elif trig > 0:
+        offset = 1
+    elif trig < -4.9:
+        offset = -4
+    elif trig < -3.9:
+        offset = -3
+    elif trig < -2.9:
+        offset = -2
+    elif trig < 0:
+        offset = -1
+
+    if offset != 0:
+        # print("trigger=%.3fV, offset=%d" % (trig, offset))
         active_gate = (active_gate + offset) % num_outs
         for index in range(num_outs):
-            write_out(index, index == active_gate)
+            output(index, index == active_gate)
