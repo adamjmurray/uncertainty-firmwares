aa967022c1e54611c563d121ee337319ad1fed89
Adam Murray <adamjmurray@gmail.com>
2023-06-16 10:29:53 -0700

    enum is not supported in micropython

--- micropython/lib/dsp.py
+++ micropython/lib/dsp.py
@@ -1,6 +1,3 @@
-from enum import Enum
-
-
 class SimpleLPF:
     def __init__(self, weight=0.5):
         self.weight = weight
@@ -12,14 +9,17 @@ class SimpleLPF:
 
 
 class BipolarTrigger:
-    State = Enum('BipolarTriggerState', ['OFF', 'RISING', 'ON', 'FALLING'])
+    STATE_OFF = "OFF"
+    STATE_RISING = "RISING"
+    STATE_ON = "ON"
+    STATE_FALLING = "FALLING"
 
     def __init__(self, rising_thresh=0.2, falling_thresh=0.1, rising_window=100, falling_window=100, lpf_weight=0.1):
         self.rising_thresh = rising_thresh
         self.falling_thresh = falling_thresh
         self.rising_window = rising_window
         self.falling_window = falling_window
-        self.state = BipolarTrigger.State.OFF
+        self.state = BipolarTrigger.STATE_OFF
         self.countdown = 0
         self.lpf = SimpleLPF(lpf_weight)
 
@@ -28,34 +28,34 @@ class BipolarTrigger:
         filtered = self.lpf.filter(sample)
         absval = abs(filtered)
 
-        if self.state == BipolarTrigger.State.OFF:
+        if self.state == BipolarTrigger.STATE_OFF:
             if absval >= self.rising_thresh:
-                self.state = BipolarTrigger.State.RISING
+                self.state = BipolarTrigger.STATE_RISING
                 self.countdown = self.rising_window
 
-        elif self.state == BipolarTrigger.State.RISING:
+        elif self.state == BipolarTrigger.STATE_RISING:
             if absval >= self.rising_thresh:
                 self.countdown -= 1
                 if self.countdown <= 0:
-                    self.state = BipolarTrigger.State.ON
+                    self.state = BipolarTrigger.STATE_ON
                     # the only time this returns anything other than zero is the moment we transition to the ON state:
                     return filtered
 
             else:
-                self.state = BipolarTrigger.State.OFF
+                self.state = BipolarTrigger.STATE_OFF
 
-        elif self.state == BipolarTrigger.State.ON:
+        elif self.state == BipolarTrigger.STATE_ON:
             if absval <= self.falling_thresh:
-                self.state = BipolarTrigger.State.FALLING
+                self.state = BipolarTrigger.STATE_FALLING
                 self.countdown = self.falling_window
 
-        elif self.state == BipolarTrigger.State.FALLING:
+        elif self.state == BipolarTrigger.STATE_FALLING:
             if absval <= self.falling_thresh:
                 self.countdown -= 1
                 if self.countdown <= 0:
-                    self.state = BipolarTrigger.State.OFF
+                    self.state = BipolarTrigger.STATE_OFF
             else:
-                self.state = BipolarTrigger.State.ON
+                self.state = BipolarTrigger.STATE_ON
 
         # if we didn't transition to ON, we always return 0
         return 0
--- micropython/template.py
+++ micropython/template.py
@@ -6,6 +6,6 @@ machine.freq(250000000)  # 2x default
 
 while (True):
     cv = io.read_cv()
-    active_gate = abs(round(cv * io.num_outs-1))
+    active_gate = abs(round(cv * (io.num_outs-1)))
     for index in range(io.num_outs):
         io.output(index, index == active_gate)
