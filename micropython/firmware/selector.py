1162f6bf2a225703c8f7de66353ffb16dd305c36
Adam Murray <adamjmurray@gmail.com>
2023-06-18 21:57:07 -0700

    add gated version of selector firmware

--- micropython/firmware/selector.py
+++ micropython/firmware/selector.py
@@ -5,10 +5,12 @@ from ..lib.constants import NUM_OUTS
 
 class Selector:
 
-    def __init__(self):
-        self.trigger = BipolarTrigger(rising_thresh=1, lpf_weight=0)
+    def __init__(self, gated=False):
+        self.trigger = BipolarTrigger(
+            rising_thresh=1, falling_thresh=0.5, lpf_weight=0)
         self.active_gate = 0
         self.gate_indexes = list(range(NUM_OUTS))
+        self.gated = gated
 
     def process(self, volts, output, num_outs):
         if self.trigger.detect(volts) != 0:
@@ -16,3 +18,8 @@ class Selector:
             active_gates = random.sample(self.gate_indexes, num_gates)
             for index in range(num_outs):
                 output(index, index in active_gates)
+
+        elif self.gated:
+            # when gated, outputs only remain active while the input gate is high
+            for index in range(num_outs):
+                output(index, 0)
