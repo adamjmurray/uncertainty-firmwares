4969c33a8dc3ecc834b8fa4a94989b3f0b55a056
Adam Murray <adamjmurray@gmail.com>
2023-06-18 20:39:46 -0700

    avoid unnecessary calculations in stepper firmware

--- micropython/stepper.py
+++ micropython/stepper.py
@@ -12,27 +12,28 @@ while True:
     volts = 5 * read_cv()
     trig = trigger.detect(volts)
 
-    # fine-tuning a bit because read_cv() is not perfectly normalized
-    # This will advance by 1 around 2V, by 2 at 3V, by 3 at 4V, and by 4 at 5V
-    if trig > 4.7:  # TODO: why is this so low, do we need to recalibrate?
-        offset = 4
-    elif trig > 3.9:
-        offset = 3
-    elif trig > 2.9:
-        offset = 2
-    elif trig > 0:
-        offset = 1
-    elif trig < -4.9:
-        offset = -4
-    elif trig < -3.9:
-        offset = -3
-    elif trig < -2.9:
-        offset = -2
-    elif trig < 0:
-        offset = -1
+    if trig != 0:
+        # fine-tuning a bit because read_cv() is not perfectly normalized
+        # This will advance by 1 around 2V, by 2 at 3V, by 3 at 4V, and by 4 at 5V
+        if trig > 4.7:  # TODO: why is this so low, do we need to recalibrate?
+            offset = 4
+        elif trig > 3.9:
+            offset = 3
+        elif trig > 2.9:
+            offset = 2
+        elif trig > 0:
+            offset = 1
+        elif trig < -4.9:
+            offset = -4
+        elif trig < -3.9:
+            offset = -3
+        elif trig < -2.9:
+            offset = -2
+        elif trig < 0:
+            offset = -1
 
-    if offset != 0:
-        # print("trigger=%.3fV, offset=%d" % (trig, offset))
-        active_gate = (active_gate + offset) % num_outs
-        for index in range(num_outs):
-            output(index, index == active_gate)
+        if offset != 0:
+            # print("trigger=%.3fV, offset=%d" % (trig, offset))
+            active_gate = (active_gate + offset) % num_outs
+            for index in range(num_outs):
+                output(index, index == active_gate)
