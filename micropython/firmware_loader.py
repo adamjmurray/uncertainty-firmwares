dbfd64a0d9be71d1c7ee412e7738bd919d9f8b75
Adam Murray <adamjmurray@gmail.com>
2023-06-21 23:17:26 -0700

    add a warning about overclocking too high

--- micropython/firmware_loader.py
+++ micropython/firmware_loader.py
@@ -1,6 +1,6 @@
-# A template for running firmwares found in the firmware subfolder.
-# If you save this file as main.py on the Uncertainty, it will run automatically
-# when the module powers on.
+# A template for running firmwares found in the firmware subfolder. If you save
+# this file as main.py on the Uncertainty, it will run automatically when the
+# module powers on.
 
 from lib.io import read_volts, output
 from machine import Timer
@@ -10,7 +10,12 @@ import machine
 from firmware.stepper import Stepper as Firmware
 firmware = Firmware(output)
 
-machine.freq(250_000_000)  # 2. Choose processor speed. (250_000_000 is overclocked to 2x the default)
+# Overclock to 2x the default because Python isn't as fast as C.
+# WARNING: do not go much higher. Somewhere above 260_000_000 the device will
+# stop working. If you overclock too high in the main.py file, the device may
+# become unresponsive immediately after powering on. Then you probably have to
+# completely reinstall the firmware to reset the filesystem on the device.
+machine.freq(250_000_000)
 
 Timer(
     mode=Timer.PERIODIC,
